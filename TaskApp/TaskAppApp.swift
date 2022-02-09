72fbbaee84b26a890db444ace4933e707979dad0
shehanhasanga <shehanhasanga.15@cse.mrt.ac.lk>
2022-02-09 08:36:35 +0530

    Initial Commit

---
+++ TaskApp/TaskAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskAppApp.swift
+//  TaskApp
+//
+//  Created by shehan karunarathna on 2022-02-09.
+//
+
+import SwiftUI
+
+@main
+struct TaskAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
